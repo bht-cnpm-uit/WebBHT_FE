ab16f2893aae5896c94d51749db9abba44ebfa31
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-01 12:18:28 +0700

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './layouts/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            spacing: {
+                'h-header': 'var(--h-header)',
+                'p-body': 'var(--p-body)',
+            },
+            maxWidth: {
+                container: 'var(--max-container)',
+            },
+            colors: {
+                primary: 'hsl(var(--clr-primary) / <alpha-value>)',
+                'primary-dark': 'hsl(var(--clr-primary-dark) / <alpha-value>)',
+                'primary-light': 'hsl(var(--clr-primary-light) / <alpha-value>)',
+                text: 'hsl(var(--clr-text) / <alpha-value>)',
+                'text-dark': 'hsl(var(--clr-text-dark) / <alpha-value>)',
+                white: 'hsl(var(--clr-white) / <alpha-value>)',
+                transparent: 'transparent',
+                current: 'currentColor',
+            },
+            zIndex: {},
+        },
+        screens: {
+            xxl: { min: '1400px' },
+            xl: { max: '1399px' },
+            lg: { max: '1199px' },
+            md: { max: '991px' },
+            sm: { max: '767px' },
+            xs: { max: '575px' },
+            'can-hover': { raw: '(hover: hover)' },
+            'cannot-hover': { raw: '(hover: none)' },
+        },
+    },
+    plugins: [],
+};
