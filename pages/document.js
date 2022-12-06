dd57fcdd49d449beb175579279f04ed286cb0a8f
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-06 11:01:50 +0700

    breadcrumb document page

--- pages/document.js
+++ pages/document.js
@@ -1,4 +1,6 @@
 import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleRight, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import Button from '../components/Button';
 import DefaultLayout from '../layouts/DefaultLayout';
 
@@ -15,6 +17,27 @@ function About() {
                 <h2 className="px-6 text-center text-xl font-semibold text-white">
                     TÀI LIỆU HỌC TẬP
                 </h2>
+            </div>
+
+            {/* Breadcrumb */}
+            <div className="flex p-4">
+                <div className="flex items-center">
+                    <button className="mx-1 rounded py-1 px-2 text-lg hover:bg-gray-100">
+                        Thư mục gốc
+                    </button>
+                    <FontAwesomeIcon icon={faChevronRight} className="text-xs" />
+                </div>
+                <div className="flex items-center">
+                    <button className="mx-1 rounded py-1 px-2 text-lg hover:bg-gray-100">
+                        Thư mục con
+                    </button>
+                    <FontAwesomeIcon icon={faChevronRight} className="text-xs" />
+                </div>
+                <div className="flex items-center">
+                    <button className="pointer-events-none mx-1 rounded py-1 px-2 text-lg font-medium hover:bg-gray-100">
+                        Thư mục con nữa
+                    </button>
+                </div>
             </div>
         </DefaultLayout>
     );
