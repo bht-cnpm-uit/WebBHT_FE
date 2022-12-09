3eb165749f34bd34072e12fc265a1aab09177fcc
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-09 16:21:47 +0700

    [about page] small update size and spacing of member card

--- components/MemberCard/MemberCard.js
+++ components/MemberCard/MemberCard.js
@@ -1,6 +1,6 @@
 function MemberCard({ member }) {
     return (
-        <div className="m-2 h-32 w-32 overflow-hidden rounded-full sm:h-24 sm:w-24">
+        <div className="m-3 h-24  w-24 cursor-pointer overflow-hidden rounded-full sm:m-2 sm:h-20 sm:w-20">
             <img src={member.image} className="h-full w-full object-cover" />
         </div>
     );
